017c3f16e360ff8d426833e678f4409e6613abbd
Görkem Eldeniz <82877261+GorkemEldeniz@users.noreply.github.com>
2024-03-30 15:51:59 +0300

    fix

--- src/pages/Home/Search/index.tsx
+++ src/pages/Home/Search/index.tsx
@@ -4,10 +4,10 @@ import SearchInput from "./SearchInput";
 
 export default function SearchWrapper() {
 	return (
-		<div className='w-full h-full px-8 bg-image'>
+		<main className='w-full h-screen px-8 bg-image'>
 			<Header />
 			<Hero />
 			<SearchInput />
-		</div>
+		</main>
 	);
 }
