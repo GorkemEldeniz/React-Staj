1f19907d68665c0916cf3839318aee99b0a071d7
Görkem Eldeniz <82877261+GorkemEldeniz@users.noreply.github.com>
2024-04-02 11:59:03 +0300

    update footer

--- src/pages/Detail/Footer.tsx
+++ src/pages/Detail/Footer.tsx
@@ -1,21 +1,30 @@
 import { setWeather } from "@/lib/Redux/features/location/locationSlice";
+import { ArrowFatLeft, House } from "@phosphor-icons/react";
 import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { redirect, useNavigate } from "react-router-dom";
 
 export default function DetailFooter() {
 	const navigate = useNavigate();
 	const dispatch = useDispatch();
 
 	return (
-		<footer className='flex justify-end'>
+		<footer className='flex justify-between'>
 			<button
 				onClick={() => {
-					dispatch(setWeather(undefined));
-					navigate("/");
+					navigate(-1);
 				}}
 				className='inline-block px-4 py-2 text-gray-100 bg-gray-700 rounded-md cursor-pointer hover:opacity-50'
 			>
-				Go Home
+				<ArrowFatLeft size={24} />
+			</button>
+			<button
+				onClick={() => {
+					dispatch(setWeather(undefined));
+					redirect("/");
+				}}
+				className='inline-block px-4 py-2 text-gray-100 bg-gray-700 rounded-md cursor-pointer hover:opacity-50'
+			>
+				<House size={24} />
 			</button>
 		</footer>
 	);
