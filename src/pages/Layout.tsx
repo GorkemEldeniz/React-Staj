cfa6d7a7fc6f74de24d13b92b2bdf3df87c36e89
Görkem Eldeniz <82877261+GorkemEldeniz@users.noreply.github.com>
2024-04-02 18:03:44 +0300

    fix

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,8 +1,10 @@
+import { Analytics } from "@vercel/analytics/react";
 import { Outlet, ScrollRestoration } from "react-router-dom";
 
 export default function Layout() {
 	return (
 		<div className='w-full max-w-screen-sm mx-auto'>
+			<Analytics />
 			<ScrollRestoration />
 			<Outlet />
 		</div>
