2aa30116f2f3a3c171b53b38d01bc6d0ce80f073
Görkem Eldeniz <82877261+GorkemEldeniz@users.noreply.github.com>
2024-04-10 11:18:55 +0300

    fix

--- src/pages/Home/Weather/Footer.tsx
+++ src/pages/Home/Weather/Footer.tsx
@@ -1,8 +1,20 @@
+import { setWeather } from "@/lib/Redux/features/location/locationSlice";
+import { House } from "@phosphor-icons/react";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 
 export default function WeatherFooter() {
+	const dispatch = useDispatch();
 	return (
-		<footer className='flex justify-end'>
+		<footer className='flex justify-between'>
+			<button
+				className='inline-block px-4 py-2 text-gray-100 bg-gray-700 rounded-md cursor-pointer hover:opacity-50'
+				onClick={() => {
+					dispatch(setWeather(undefined));
+				}}
+			>
+				<House size={24} />
+			</button>
 			<Link
 				to='detail'
 				className='inline-block px-4 py-2 text-gray-100 bg-gray-700 rounded-md cursor-pointer hover:opacity-50'
--- src/pages/Home/Weather/ForecastList.tsx
+++ src/pages/Home/Weather/ForecastList.tsx
@@ -1,4 +1,5 @@
-import { getWeekdayAbbreviation } from "@/helpers";
+import { getTimePeriodBasedOnSunset, getWeekdayAbbreviation } from "@/helpers";
+import { imagesMap } from "./images-map";
 import type { List } from "./type";
 
 export default function WeatherForecastList({
@@ -12,16 +13,33 @@ export default function WeatherForecastList({
 		getWeekdayAbbreviation(new Date(l.dt * 1000 + timezone * 1000))
 	);
 
+	const getWeatherForecastIcon = (index: number) => {
+		const { weather, sunset } = list[index];
+		const { main, description } = weather[0];
+
+		const timePeriod = getTimePeriodBasedOnSunset(sunset);
+
+		return imagesMap.find(
+			({ weather, descriptions, period }) =>
+				weather === main &&
+				descriptions.includes(description) &&
+				timePeriod === period
+		);
+	};
+
 	return (
 		<section className='bg-gray-700 rounded-md'>
 			<ol className='flex justify-between p-3'>
 				{list.map((l, index) => (
-					<li key={index} className='px-[5.5px] py-[14px] text-center'>
+					<li
+						key={index}
+						className='px-[5.5px] py-[14px] text-center flex flex-col gap-1 items-center justify-center'
+					>
 						<h3 className='text-gray-200 text-heading-xs'>{days[index]}</h3>
 						<img
-							className='size-14'
-							src={`https://openweathermap.org/img/w/${l.weather[0].icon}.png`}
-							alt=''
+							className='object-contain size-14'
+							src={getWeatherForecastIcon(index)?.icon_path}
+							alt={getWeatherForecastIcon(index)?.weather}
 						/>
 						<div className='text-heading-xs'>
 							<span className='text-gray-100'>
